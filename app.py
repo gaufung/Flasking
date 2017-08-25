9cf1136e9c912f6952c95e7ec8e654abed30d7ad
gaufung <gaufung@outlook.com>
2017-08-25 15:31:02 +0800

    use session

--- app.py
+++ app.py
@@ -26,14 +26,11 @@ def internet_server_error(e):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data=''
-        #session['name']= form.name.data
-        #return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=name)
+        session['name']= form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
