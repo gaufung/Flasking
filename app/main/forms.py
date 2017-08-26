54b46899a7db24e47b3677e63e4147320b63229f
gaufung <gaufung@outlook.com>
2017-08-26 22:57:56 +0800

    modify index page

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import Required, Regexp, EqualTo
 
 
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+# class NameForm(FlaskForm):
+#     name = StringField('What is your name?', validators=[Required()])
+#     submit = SubmitField('Submit')
 
 
--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from threading import Thread
 from flask import render_template, session, redirect, url_for,abort
 from flask_mail import Message
 from . import main
-from .forms import NameForm
+# from .forms import NameForm
 from .. import db, mail
 from ..models import User
 from ..decorators import admin_required, permisson_required
@@ -23,26 +23,30 @@ from flask_login import login_required
 #     t.start()
 #     return t
 
-@main.route('/', methods=['GET', 'POST'])
+# @main.route('/', methods=['GET', 'POST'])
+# def index():
+#     form = NameForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.name.data).first()
+#         if user is None:
+#             user = User(username=form.name.data)
+#             db.session.add(user)
+#             db.session.commit()
+#             session['known']=False
+#             # if app.config['FLASKY_ADMIN']:
+#             #     send_email(app.config['FLASKY_ADMIN'], 'New User', 
+#             #     'mail/new_user', user=user)
+#         else:
+#             session['known']=True
+#         session['name']=form.name.data
+#         form.name.data=''
+#         return redirect(url_for('main.index'))
+#     return render_template('index.html', form=form, name=session.get('name'),
+#             known=session.get('known',False))
+
+@main.route('/')
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known']=False
-            # if app.config['FLASKY_ADMIN']:
-            #     send_email(app.config['FLASKY_ADMIN'], 'New User', 
-            #     'mail/new_user', user=user)
-        else:
-            session['known']=True
-        session['name']=form.name.data
-        form.name.data=''
-        return redirect(url_for('main.index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-            known=session.get('known',False))
+    return render_template('index.html')
 
 @main.route('/user/<username>')
 def user(username):
