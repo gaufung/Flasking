26ec087b5243d4efbbcbde47d83c474f40679ede
gaufung <gaufung@outlook.com>
2017-08-27 09:10:01 +0800

    add avatar

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
 from  datetime import datetime
+import hashlib
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
-from flask import current_app
+from flask import current_app, request
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
@@ -84,6 +85,15 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size=100, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
 
     # def generate_confirmation_token(self, expiration=3600):
     #     s = Serializer(current_app.config['SECRET_KEY'], expiration)
